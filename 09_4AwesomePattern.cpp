11adcda21d70e4bf88fef57e553d1bff30c1423a
himanshu-singh07 <himanshuwch@gamil.com>
2022-09-15 23:33:25 +0530

    First Commit

---
+++ 09_4AwesomePattern.cpp
@@ -0,0 +1,20 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int i,j,n;
+    cin>>n;
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(n-i>=j){
+                cout<<" ";
+
+            }
+            else{
+                cout<<"*";
+            }
+
+        }
+        cout<<endl;
+    }
+ }
