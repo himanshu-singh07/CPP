47db2a20e34e49e5ef46bc6975ed3f040c2bf8d8
himanshu-singh07 <himanshuwch@gamil.com>
2022-09-25 01:04:08 +0530

    First Commit

---
+++ 13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void print(int num){
+
+    cout<<num<<endl;
+    return;
+}
+int add(int num1, int num2){
+    int sum = num1 + num2;
+    return sum;
+}
+int main(){
+    int a=2, b=3;
+    cout<<add(a,b)<<endl;
+    return 0;
+}
